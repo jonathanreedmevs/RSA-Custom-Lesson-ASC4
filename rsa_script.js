8c7eff5c07581484493151b43f92e55a772f1184
jonathan <jrmevs@umd.edu>
2017-08-10 10:57:52 -0400

    explicitly outlining the secure communication path

--- rsa_script.js
+++ rsa_script.js
@@ -20,69 +20,116 @@ var bigInt = require("big-integer");
 var primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101];
 
 //necessary variables
-var prime1, prime2, thanksEuler, publicLargePrime, publicEncodingExponent, privateDecodingExponent;
-
-function generateKey(){
-    
-}
+var publicLargePrime, publicEncodingExponent;
 
 function pickTwoPrimes() {
-    prime1 = primes[Math.floor(Math.random() * primes.length)];
-    prime2 = primes[Math.floor(Math.random() * primes.length)];
+    var prime1 = primes[Math.floor(Math.random() * primes.length)];
+    var prime2 = primes[Math.floor(Math.random() * primes.length)];
+    while(prime1 == prime2){
+        prime1 = primes[Math.floor(Math.random() * primes.length)];
+        prime2 = primes[Math.floor(Math.random() * primes.length)];   
+    }
+    return [prime1, prime2];
 }
 
-function generatePublicAndPrivateDependency() {
-    publicLargePrime = prime1 * prime2;
-    thanksEuler = phi(prime1 * prime2);
-    thanksEuler = (prime1 - 1) * (prime2 - 1);
-}
-
-function generatePublicEncodingExponentAndPrivateDecoding() {
+function generatePublicEncodingExponentAndPrivateDecoding(thanksEuler) {
     var workingInt = thanksEuler + 1;
     while (!(inverseMod(workingInt, thanksEuler) == 1 && factor(workingInt).length != 1)) {
         workingInt++;
     }
-    publicEncodingExponent = factor(workingInt)[0].prime;
-    privateDecodingExponent = factor(workingInt)[1].prime;
+    var encoding = factor(workingInt)[0].prime;
+    var decoding = factor(workingInt)[1].prime;
+    var obj = {
+        encodingExponent: encoding,
+        decodingExponent: decoding
+    }
+    return obj;
 }
 
-function checkMySelf(){
-    console.log("p: " + prime1);
-    console.log("q: " + prime2);
-    console.log("N: " + publicLargePrime);
-    console.log("r: " + thanksEuler);
-    console.log("e: " + publicEncodingExponent);
-    console.log("d: " + privateDecodingExponent);
-}
 
 function encodeDecodeDisplay(str) {
     var message = ""
     var strToUse = str.toUpperCase();
     for (var i = 0; i < str.length; i++) {
         message = message + str.charCodeAt(i);
-    } 
+    }
     message = parseInt(message);
     var whatYouSend = encode(message);
     var onlyICanFigureThisOut = decode(whatYouSend, privateDecodingExponent);
     console.log(onlyICanFigureThisOut);
 }
 
-function encode(message){
-    return bigInt(message).pow(publicEncodingExponent).mod(publicLargePrime);
-}
-
-function decode(message, privateDecodingExponent){
-    var str = "";
-    var decodedNum = bigInt(message).pow(privateDecodingExponent).mod(publicLargePrime);
-    var workingString = decodedNum.value.toString();
-    for(var i = 0; i < workingString.length; i +=2){
-        str = str + String.fromCharCode(workingString.slice(i, i+2));
+function Sender() {
+    this.send = function (str) {
+        var message = "";
+        var strToUse = str.toUpperCase();
+        for (var i = 0; i < str.length; i++) {
+            message = message + str.charCodeAt(i);
+        }
+        message = parseInt(message);
+        console.log(message);
+        var encoded = bigInt(message).pow(publicEncodingExponent).mod(publicLargePrime);
+        return encoded;
     }
-    return str;
 }
 
 
-pickTwoPrimes();
-generatePublicAndPrivateDependency();
-generatePublicEncodingExponentAndPrivateDecoding();
-encodeDecodeDisplay("a");
+function Receiver() {
+    this.twoPrimes = pickTwoPrimes();
+    this.publicLargePrime = this.twoPrimes[0] * this.twoPrimes[1];
+    var thanksEuler = phi(this.publicLargePrime);
+    var exponents = generatePublicEncodingExponentAndPrivateDecoding(thanksEuler);
+    this.publicEncodingExponent = exponents.encodingExponent;
+    this.decodingExponent = exponents.decodingExponent;
+
+    this.publishPublicKey = function () {
+        publicLargePrime = this.publicLargePrime;
+        publicEncodingExponent = this.publicEncodingExponent;
+    }
+
+    this.receive = function (message) {
+        var str = "";
+        var decodedNum = bigInt(message).pow(this.decodingExponent).mod(publicLargePrime);
+        var workingString = decodedNum.value.toString();
+        for (var i = 0; i < workingString.length; i += 2) {
+            str = str + String.fromCharCode(workingString.slice(i, i + 2));
+        }
+        return str;
+    }
+}
+
+function traceSecureCommunication(receiver, sender){
+    console.log("Two primes generated: Only the receiver knows this. " +
+            "Very secure because this takes FOREVER for a computer to figure out.");
+    console.log("Prime1: " + receiver.twoPrimes[0]);
+    console.log("Prime2: " + receiver.twoPrimes[1]);
+
+    console.log("Public large prime to be published: "  + receiver.publicLargePrime);
+
+    console.log("Now, through a terrible brute-force homemade algorithm, we find" + 
+            " a number that is both 1 mod phi of the large prime, and factors into two primes. " +
+            "This will generate our encoding and decoding exponent. Through factorization");
+
+    console.log("Encoding exponent: " + receiver.publicEncodingExponent);
+    console.log("Decoding exponent (No one sees this!): " + receiver.decodingExponent);
+
+    console.log("Now to \"publish\" the public key");
+    receiver.publishPublicKey();
+
+    console.log("Now the sender will encrypt, and send a message. Using the " + 
+            "\"public\" info published by the receiver");
+    
+    var message = "A"
+    console.log("Original message (numerical): " + message.charAt(0));
+    var encoded = sender.send(message);
+    console.log("Encoded message: " + encoded);
+
+    var received = receiver.receive(encoded);
+    console.log("Decoded message: " + received);
+
+}
+
+
+var Jon = new Receiver();
+var Oscar = new Sender();
+traceSecureCommunication(Jon, Oscar);
